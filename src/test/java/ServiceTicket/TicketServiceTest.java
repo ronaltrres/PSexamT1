884cabe5a72f851c729a983104c18137333d4e5a
ronaltrres <ronaltrres@gmail.com>
2026-03-30 20:19:50 -0500

    tes2 mejorado

--- src/test/java/ServiceTicket/TicketServiceTest.java
+++ src/test/java/ServiceTicket/TicketServiceTest.java
@@ -2,6 +2,7 @@ package ServiceTicket;
 
 import EntityTicket.Ticket;
 import EntityTicket.Usuario;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mock;
@@ -12,21 +13,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class TicketServiceTest {
 
     @Test
+    @DisplayName("No debe permitir más de 5 tickets abiertos")
     void noDebePermitirMasDe5Tickets() {
 
-
-        Usuario usuarioMock = Mockito.mock(Usuario.class) ;
+        // Arrange (preparar)
+        Usuario usuarioMock = Mockito.mock(Usuario.class);
         Mockito.when(usuarioMock.getTicketsAbiertos()).thenReturn(5);
 
-
         Ticket t = new Ticket();
-        t.setCodigo("t001");
+        t.setCodigo("T001");
         t.setPrioridad("media");
 
         TicketService service = new TicketService();
 
+
         String resultado = service.crearTicket(t, usuarioMock);
+
 
         assertEquals("Máximo de tickets abiertos alcanzado", resultado);
     }
+
+
 }
